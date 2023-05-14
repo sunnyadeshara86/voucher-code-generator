88b6ca5c7460c04799c26b6f69a88a8b16a890d0
sunny.a.adeshara@outlook.com <sunnyadeshara@outlook.com>
2023-05-14 11:31:26 +0530

    Created library to generate voucher code using various configuration and unit tests for the library

---
+++ VoucherCodeGenerator/VoucherCodeGenerator.Lib/CharactersSet.cs
@@ -0,0 +1,9 @@
+﻿namespace VoucherCodeGenerator.Lib
+{
+    public static class CharactersSet
+    {
+        public static string ALPHABETIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static string ALPHANUMERIC = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static string NUMBERS = "0123456789";
+    }
+}
