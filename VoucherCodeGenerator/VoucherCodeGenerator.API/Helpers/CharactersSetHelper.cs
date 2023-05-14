e6573fe81984c4eca05613a6091ef823d804090e
sunny.a.adeshara@outlook.com <sunnyadeshara@outlook.com>
2023-05-14 12:13:42 +0530

    Created controller and action to generate voucher code

---
+++ VoucherCodeGenerator/VoucherCodeGenerator.API/Helpers/CharactersSetHelper.cs
@@ -0,0 +1,27 @@
+﻿using VoucherCodeGenerator.Lib;
+
+namespace VoucherCodeGenerator.API.Helpers
+{
+    public static class CharactersSetHelper
+    {
+        public static string GetCharactersSetFromName(string name)
+        {
+            string charactersSet;
+
+            switch (name.ToUpperInvariant())
+            {
+                case "ALPHABETIC":
+                    charactersSet = CharactersSet.ALPHABETIC;
+                    break;
+                case "ALPHANUMERIC":
+                    charactersSet = CharactersSet.ALPHANUMERIC;
+                    break;
+                default:
+                    charactersSet = CharactersSet.NUMBERS;
+                    break;
+            }
+
+            return charactersSet;
+        }
+    }
+}
---
+++ VoucherCodeGenerator/VoucherCodeGenerator.API/Models/GenerateVoucherCodeResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace VoucherCodeGenerator.API.Models
+{
+    public class GenerateVoucherCodeResponse
+    {
+        public string VoucherCode { get; set; } = string.Empty;
+    }
+}
